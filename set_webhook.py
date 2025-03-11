70c6285c08047a15f0e8cfb05007eb7b6d2837d9
Mohamed-945 <moalex94@gmail.com>
2025-03-11 20:41:54 +0200

    Update set_webhook.py

--- set_webhook.py
+++ set_webhook.py
@@ -1,11 +1,14 @@
-# set_webhook.py
 import asyncio
-from telegram_bot import bot
+from aiogram import Bot
+from aiogram.client.default import DefaultBotProperties
 
-async def setup_webhook():
-    webhook_url = "https://telegram-bot.onrender.com/7803240855:AAEKbgY2IV3WOETp12oCtt5d-Hvl42mWDpU"
+TOKEN = "7803240855:AAEKbgY2IV3WOETp12oCtt5d-Hvl42mWDpU"
+bot = Bot(token=TOKEN, default=DefaultBotProperties(parse_mode="HTML"))
+
+async def setup():
+    webhook_url = f"https://telegram-bot.onrender.com/{TOKEN}"  # تأكد من الرابط الصحيح
     await bot.set_webhook(webhook_url)
-    print("✅ Webhook Set Successfully!")
+    print(f"✅ Webhook تم ضبطه: {webhook_url}")
 
-if __name__ == "__main__":
-    asyncio.run(setup_webhook())
+if __name__ == '__main__':
+    asyncio.run(setup())
