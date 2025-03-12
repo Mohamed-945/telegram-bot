8208c9ed84079e78980422b14f7548404174559d
Mohamed-945 <moalex94@gmail.com>
2025-03-12 02:22:50 +0200

    Update set_webhook.py

--- set_webhook.py
+++ set_webhook.py
@@ -1,14 +1,1 @@
-import asyncio
-from aiogram import Bot
 
-TOKEN = "7803240855:AAEKbgY2IV3WOETp12oCtt5d-Hvl42mWDpU"
-WEBHOOK_URL = "https://telegram-bot-c2uv.onrender.com/7803240855:AAEKbgY2IV3WOETp12oCtt5d-Hvl42mWDpU"  # ✅ رابط صحيح مع التوكن
-
-bot = Bot(token=TOKEN)
-
-async def set_webhook():
-    await bot.set_webhook(WEBHOOK_URL)
-    print("✅ Webhook تم ضبطه بنجاح!")
-
-if __name__ == "__main__":
-    asyncio.run(set_webhook())
