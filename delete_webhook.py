97ffd8fd77e07100236f841b9aec95ceb340ed98
Mohamed-945 <moalex94@gmail.com>
2025-03-12 02:38:53 +0200

    Update delete_webhook.py

--- delete_webhook.py
+++ delete_webhook.py
@@ -1,14 +1,1 @@
-import asyncio
-from aiogram import Bot
-
-TOKEN = "7803240855:AAEKbgY2IV3WOETp12oCtt5d-Hvl42mWDpU"  # ضع التوكن الصحيح
-
-bot = Bot(token=TOKEN)
-
-async def delete_webhook():
-    await bot.delete_webhook(drop_pending_updates=True)
-    print("✅ Webhook تم حذفه بنجاح!")
-
-if __name__ == "__main__":
-    asyncio.run(delete_webhook())
 
